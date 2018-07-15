e12a6e32d688138a6e5ec08fb9efab0f3943a57c
Folrunge <folrunge@gmail.com>
2018-07-15 17:06:36 +0200

    added py comments

--- application.py
+++ application.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template, flash, redirect, request, url_for
 from datetime import datetime
 
 app = Flask(__name__, template_folder='templates')
+#Array for holding navbar names
 names = ["Home", "About me", "Projects", "Hobby"]
 
 @app.route("/")
 def index():
+	#Function for getting local server time
 	time = datetime.now().time()
 	if time.hour > 0 and time.hour < 12:
 		headline = "Good morning,  !"
@@ -13,4 +15,5 @@ def index():
 		headline = "Good afternoon,  !"
 	else:
 		headline = "Good evening,  !"
+	#Return for template with headers and ul> li	
 	return render_template("index.html", headline=headline, names=names)
