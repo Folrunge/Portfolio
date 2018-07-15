251e79f582d4ecb98b32397b1b417c3e77a24123
Folrunge <folrunge@gmail.com>
2018-07-15 15:14:42 +0200

    Modified python and jinja2

--- application.py
+++ application.py
@@ -1,18 +1,16 @@
 from flask import Flask, render_template, flash, redirect, request, url_for
 from datetime import datetime
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+names = ["Home", "About me", "Projects", "Hobby"]
 
 @app.route("/")
 def index():
 	time = datetime.now().time()
-	if time.hour > (0) and time.hour < (12):
-		headline1 = "Good morning,  !"
-		return render_template("index.html", headline1=headline1)
-	elif time.hour >= (12) and time.hour < (18):
-		headline2 = "Good afternoon,  !"
-		return render_template("index.html", headline2=headline2)
+	if time.hour > 0 and time.hour < 12:
+		headline = "Good morning,  !"
+	elif time.hour >= 12 and time.hour < 18:
+		headline = "Good afternoon,  !"
 	else:
-		headline3 = "Good evening,  !"
-		return render_template("index.html", headline3=headline3)
-	
+		headline = "Good evening,  !"
+	return render_template("index.html", headline=headline, names=names)
