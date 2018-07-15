41b0bf39bc91fd465cf070df06f7a0d08de2bd57
Folrunge <folrunge@gmail.com>
2018-07-15 11:22:03 +0200

    Timetracking and time based greeting added

--- application.py
+++ application.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template, flash, redirect, request, url_for
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	headline1 = "Good morning,  !"
-	headline2 = "Good afternoon,  !"
-	headline3 = "Good evening,  !"
-	return render_template("index.html", headline1=headline1)
+	time = datetime.now().time()
+	if time.hour > (0) and time.hour < (12):
+		headline1 = "Good morning,  !"
+		return render_template("index.html", headline1=headline1)
+	elif time.hour >= (12) and time.hour < (18):
+		headline2 = "Good afternoon,  !"
+		return render_template("index.html", headline2=headline2)
+	else:
+		headline3 = "Good evening,  !"
+		return render_template("index.html", headline3=headline3)
+	
