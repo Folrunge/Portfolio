13d05ed58ecf5794c300561e0c03abe13f7258e1
Folrunge <folrunge@gmail.com>
2018-07-21 23:39:19 +0200

    various changes

---
+++ static/front.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	$("#Home").prepend('<i class="fa fa-home fa-2x"></i>');
+	$("#About").prepend('<i class="fa fa-user fa-2x"></i>');
+	$("#Projects").prepend('<i class="fa fa-code fa-2x"></i>');
+	$("#Hobby").prepend('<i class="fa fa-gamepad fa-2x" style></i>');
+})
